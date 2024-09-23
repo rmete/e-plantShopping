6a086fd681c29950d56bf6308d643c162a9ad9b9
Riley Mete <ramete2@illinois.edu>
2024-09-23 01:05:57 -0400

    Increment button is working

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -221,6 +221,26 @@ function ProductList() {
     }
 ];
 
+const styleObj={
+    backgroundColor: '#4CAF50',
+    color: '#fff!important',
+    padding: '15px',
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignIems: 'center',
+    fontSize: '20px',
+   }
+   const styleObjUl={
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: '1100px',
+   }
+   const styleA={
+    color: 'white',
+    fontSize: '30px',
+    textDecoration: 'none',
+   }
   useEffect(() => {
     // Sync with cartItems to handle disabling the Add to Cart button after refresh
     const cartState = {};
